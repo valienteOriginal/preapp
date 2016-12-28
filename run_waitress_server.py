f5a0b9f42ed4c5f1f5cc0c6ad46da7c011db83a6
prashanthmadi <prashanthrmadi@gmail.com>
2016-12-28 16:27:38 -0600

    adding correct port number

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -3,4 +3,4 @@ from waitress import serve
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosql.settings")
 application = get_wsgi_application()
-serve(application, listen=os.environ["PORT"])
+serve(application,host="0.0.0.0",port=os.environ["PORT"])
